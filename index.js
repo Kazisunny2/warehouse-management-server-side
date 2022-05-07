b45167423601dc62652c55244e8e832058cfb3fd
Kazisunny2 <saidul.bit@gmail.com>
2022-05-07 15:35:16 +0200

    delete added

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ async function run() {
       const result = await inventoryCollection.insertOne(newInventory);
       res.send(result);
     });
+
+    //DELETE
+    app.delete("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
